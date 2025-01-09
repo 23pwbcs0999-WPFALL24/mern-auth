859ddf33b79b9c7db174b19f6f1aa023d3096c9b
23pwbcs0999 <23pwbcs0999@uetpeshawar.edu.pk>
2025-01-09 19:25:32 +0500

    COMMIT DONE

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/auth'); // Importing routes from the 'auth' module to handle authentication-related API endpoints.
+const authRoutes = require('./routes/auth');
 require('dotenv').config();
 
-
 const app = express();
-
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-
-
 app.use(express.json());
 
 
-app.use('/api', authRoutes);
-// Fallback route for root access
 app.get('/', (req, res) => {
-    res.send('Welcome to the MERN Auth Backend! Use /api/signup, /api/signin, or /api/protected to interact with the API.');
+    res.send('Welcome to the MERN Auth Backend!');
 });
 
-// Handle undefined routes
+
+app.get('/api/signup', (req, res) => {
+    res.send('Welcome to the Signup Endpoint! ');
+});
+
+
+app.get('/api/signin', (req, res) => {
+    res.send('Welcome to the Signin Endpoint! ');
+});
+
+
+app.use('/api', authRoutes);
+
+
 app.use((req, res) => {
     res.status(404).send('Error 404: Route not found. Please use the correct API endpoints.');
 });
 
 // MongoDB connection
-
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
-            console.log("mongodb connected successfully");
+            console.log("MongoDB connected successfully");
         });
     })
     .catch(err => console.error(err));
-
